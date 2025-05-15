c818da9c62291485a90059234b03a15b341d57a7
riyadhullah <md.riyadhullah1@gmail.com>
2025-05-15 20:39:25 +0600

    Initial commit

---
+++ ComillaCentralMedical/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ComillaCentralMedical
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ComillaCentralMedical/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ComillaCentralMedical.MvcApplication" Language="C#" %>
