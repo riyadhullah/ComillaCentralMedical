f177ec0726d13505b78cb90e008661e8edfdd896
Jahidul haque fahad <jahidulh829@gmail.com>
2025-06-21 20:49:03 +0600

    Admin Dashboard add

---
+++ ComillaCentralMedical/Controllers/AdminDashboardController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ComillaCentralMedical.Context;
+using ComillaCentralMedical.Models;
+
+
+namespace ComillaCentralMedical.Controllers
+{
+    public class AdminDashboardController : Controller
+    {
+        public MedicalDbContext db;
+        public AdminDashboardController()
+        {
+            this.db = new MedicalDbContext();
+        }
+
+        // GET: Admin/Dashboard
+        public ActionResult Dashboard()
+        {
+            var totalPatients = 20; //db.Patients.Count();
+            var totalInvoices = 25; //db.Invoices.Count();
+            var totalIncome = 35000; //db.Invoices.Sum(i => (decimal?)i.Total) ?? 0;
+            /*var recentActivities = db.ActivityLogs
+                .OrderByDescending(a => a.Timestamp)
+                .Take(5)
+                .Select(a => new { a.Action, a.Timestamp, a.User.FullName })
+                .ToList();*/
+
+            ViewBag.TotalPatients = totalPatients;
+            ViewBag.TotalInvoices = totalInvoices;
+            ViewBag.TotalIncome = totalIncome;
+           // ViewBag.RecentActivities = recentActivities;
+
+            return View();
+        }
+    }
+}
